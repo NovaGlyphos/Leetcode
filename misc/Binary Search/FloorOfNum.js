5126eb6756efebb77dd12fb4cef84cf9c62a996e
Ayush <ag13015002@gmail.com>
2025-06-09 02:47:21 +0530

    Floor

---
+++ misc/Binary Search/FloorOfNum.js
@@ -0,0 +1,23 @@
+
+const Floor = (arr,target) => {
+    let start = 0;
+    let end = arr.length - 1;
+    while(start <= end){
+        let mid = Math.floor((start+end)/2);
+        if(target === arr[mid]){
+            return mid;
+        }
+        else if(target > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return end;
+}
+
+
+const arr = [2,3,5,9,14,16,18];
+const result = Floor(arr,17);
+console.log(result);
