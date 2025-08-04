aab9fe3a7ae36621f605367477ff0e4be1c8157f
Ayush <ag13015002@gmail.com>
2025-08-04 12:18:08 +0530

    34

--- 34-FirstAndLastPosElement.js
+++ 34-FirstAndLastPosElement.js
@@ -1,6 +1,48 @@
+
 var searchRange = function(nums, target) {
-    
+    const findLeftMost = (nums,target) => {
+        let left = 0;
+        let right = nums.length-1;
+        let left_most = -1;
+        while(left <= right){
+            let mid = Math.floor((left+right)/2)
+            if(nums[mid] === target){
+                left_most = mid //possible left_most
+                right = mid - 1;
+            }
+            else if (nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left_most;
+    }
+    const findRightMost = (nums,target) => {
+        let left = 0, right = nums.length-1;
+        let right_most = -1;
+        while(left<=right){
+            let mid = Math.floor((left+right)/2);
+            if(nums[mid] === target){
+                right_most = mid //possible right_most;
+                left = mid+1;
+            }
+            else if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return right_most;
+    }
+
+    let left_most = findLeftMost(nums,target);
+    let right_most = findRightMost(nums,target);
+
+    return [left_most,right_most];
 };
 
 
-console.log(searchRange([5,7,7,8,8,10],8));
+console.log(searchRange([0],0));
